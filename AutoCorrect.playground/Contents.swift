7ef1dc0f4584fe965b0ccf3544e8eacf9326b5f5
dev-tes <tesleem.amuda@gmail.com>
2022-03-07 15:42:20 +0100

    format code properly

--- AutoCorrect.playground/Contents.swift
+++ AutoCorrect.playground/Contents.swift
@@ -27,17 +27,30 @@ func autocorrect(_ text: String) -> String {
 }
 
 autocorrect("u are welcome")
-autocorrect("We have sent the deliverables to you.") //, "We have sent the deliverables to your client.")
-autocorrect("Our team is excited to finish this with you.") //, "Our team is excited to finish this with your client.")
-autocorrect("youtube") //, "youtube")
-autocorrect("Look forward to meeting youuuuu") //, "Look forward to meeting your client")
-autocorrect("We want to film the commercial with you and syndicate it to youtube") //, "We want to film the commercial with your client and syndicate it to youtube")
-autocorrect("You should begin on Monday") //, "your client should begin on Monday")
-autocorrect("You u youville utube you youyouyou uuu raiyou united youuuu u you") //, "your client your client youville utube your client youyouyou uuu raiyou united your client your client your client")
-autocorrect("You = so close") //, "your client = so close")
-autocorrect("The case is finished with u") //, "The case is finished with your client")
-autocorrect("Let's market you on youtube") //, "Let's market your client on youtube")
-autocorrect("Our friend Alabinyou is conveniently named, and he wants to build a website called youwin with youuu") //, "Our friend Alabinyou is conveniently named, and he wants to build a website called youwin with your client")
-autocorrect("You should be active on younation") //, "your client should be active on younation")
-autocorrect("Hope to see u there!") //, "Hope to see your client there!")
+autocorrect("We have sent the deliverables to you.")
+// "We have sent the deliverables to your client."
+autocorrect("Our team is excited to finish this with you.")
+// "Our team is excited to finish this with your client."
+autocorrect("youtube")
+// "youtube"
+autocorrect("Look forward to meeting youuuuu")
+// "Look forward to meeting your client"
+autocorrect("We want to film the commercial with you and syndicate it to youtube")
+// "We want to film the commercial with your client and syndicate it to youtube"
+autocorrect("You should begin on Monday")
+// "your client should begin on Monday"
+autocorrect("You u youville utube you youyouyou uuu raiyou united youuuu u you")
+// "your client your client youville utube your client youyouyou uuu raiyou united your client your client your client"
+autocorrect("You = so close")
+// "your client = so close"
+autocorrect("The case is finished with u")
+// "The case is finished with your client"
+autocorrect("Let's market you on youtube")
+// "Let's market your client on youtube"
+autocorrect("Our friend Alabinyou is conveniently named, and he wants to build a website called youwin with youuu")
+// "Our friend Alabinyou is conveniently named, and he wants to build a website called youwin with your client"
+autocorrect("You should be active on younation")
+// "your client should be active on younation"
+autocorrect("Hope to see u there!")
+// "Hope to see your client there!"
 
